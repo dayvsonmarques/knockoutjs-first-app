ed2a739f0dcd1d5d0ba285f8c955a2ba74552f06
Dayvson Marques <dayvson.marques@gmail.com>
2019-09-05 15:36:25 -0300

    webpack start config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import someImportedFunction from './someFunction';
+
+const testTranspile = () => console.log('I am an ES6 arrow funcion');
+
+someImportedFunction();
+testTranspile();
+
+console.log('Webpack Zero Config Works!');
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin =  require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
